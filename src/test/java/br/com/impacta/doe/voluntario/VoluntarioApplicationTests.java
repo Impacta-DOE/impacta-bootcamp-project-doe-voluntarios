43b960d43ca57448c81cc3e228a5cea05ce11b3a
Arthur <santosalmeida.arthur@gmail.com>
2022-01-06 21:39:18 -0300

    first commit

---
+++ src/test/java/br/com/impacta/doe/voluntario/VoluntarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.impacta.doe.voluntario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoluntarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
