2563091cec12c209a81385f1b49291ccb74307d2
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2022-01-10 11:03:05 -0300

    fix: adicionando cors

--- src/main/java/br/com/impacta/doe/voluntario/web/VoluntarioController.java
+++ src/main/java/br/com/impacta/doe/voluntario/web/VoluntarioController.java
@@ -3,7 +3,9 @@ package br.com.impacta.doe.voluntario.web;
 import br.com.impacta.doe.voluntario.application.service.VoluntarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = { "*" })
 @RestController
 @RequestMapping("/voluntario")
 public class VoluntarioController {
